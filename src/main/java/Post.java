b03e2f0b73a87b14f24a66c980dd454d508ac148
Mokosiej Tatsiana <mokosiejtatsiana@gmail.com>
2023-04-07 19:27:50 +0200

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public boolean subscription;
+    public String surname;
+    public FormData birthday;
+}
+
